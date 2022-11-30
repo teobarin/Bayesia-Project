a9a1c95856d1a560020d3fd237cbbfefd72a5012
mtracanella <119503147+mtracanella@users.noreply.github.com>
2022-11-30 14:24:54 +0100

    Add files via upload

--- code.R
+++ code.R
@@ -1,145 +1,223 @@
+#
+# SIMBOLI: α β λ θ ψ σ
+# 
+# λ = log(θ)     θ = exp(λ)
+#                                       exp(ψ)
+# ψ = log(σ) - log(1-σ)         σ = -------------
+#                                     1 + exp(ψ)
+#
+############################# METROPOLIS HASTINGS ##############################
+#
+# 1) k=1, define x_1
+# 2) find x* from q(x*|x_k) and calculate
+#
+#                         π(x*)q(x_k|x*)    
+#     α(x_k,x*)= min ( 1, -------------- )
+#                         π(x)q(x*|x_k)
+#
+# 3) accept x* with probability α(x_k,x*) using u ~ U(0,1):
+#    if α(x_k,x*) >= u, x_k+1 = x*
+#    if α(x_k,x*) < u, x_k+1 = x_k
+#
+# 4) set k = k+1 and go back to step 2)
+#
+################################# UPDATE SIGMA #################################
+#
+# prior density σ: σ ~ Beta(1,1)
+#
+# proposal density q(x*|x) ~ N(x,α)
+#
+#                                  k                         Г(θ/σ + k - m1_bar)
+# f(σ) = L(σ) * σ^(k-m1_bar) *     ∏    (1 - σ)_(n_i - 1) * ---------------------
+#                             i=m1_bar+1                           Г(θ/σ)
+#
+# posterior density σ: L(σ|X,θ,m1_bar) ~ L(σ)*f(σ)
+#
+# posterior density λ: log(L(σ|X,θ,m1_bar)) ~ log(L(exp(ψ)/(1 + exp(ψ))) + log(f(exp(ψ)/(1 + exp(ψ))))
+#
+################################# UPDATE THETA #################################
+#
+# prior density θ: θ ~ Gamma(2,0.02)
+#
+# proposal density q(x*|x) ~ N(x,α)
+#
+#         Г(θ) * Г(θ/σ + k - m1_bar)
+# g(θ) = ----------------------------
+#         Г(θ/σ) * Г(θ + n - m1_bar)
+#
+# posterior density θ: L(θ|X,σ,m1_bar) ~ L(θ)*g(θ)
+#
+# posterior density λ: log(L(θ|X,σ,m1_bar)) ~ log(L(exp(λ))) + log(g(exp(λ)))
+#
+################################################################################
+# RECUPERO n,k,m1_bar DA DATASET
 library(readxl)
-library(catdata)
 library(dplyr)
 data = read_excel("Dataset.xlsx")
-n=dim(data)[1]
+n = dim(data)[1]
 
-subset=count(data,verbatimScientificName)
-ind=which(subset$n==1)
-m1=length(ind)
+subset = count(data,verbatimScientificName)
+ind = which(subset$n==1)
+m1 = length(ind)
 
-N_k=subset[[2]]
-N_k=sort(N_k)
-k=length(N_k)
+N_k = subset$n
+N_k = sort(N_k)
+k = length(N_k)
+# Valori paper
+# n=131204
+# k=619
+# m1=296
 
-################################################################################
-# Algoritmo su R cos? da non dover modificare MH in Python
-
-## ALGORITHM 1
-## MH PER UPDATE DI THETA E SIGMA
-## psi = log(sigma/(1-sigma))
-## lambda = log(theta)
-
-a_theta <- 2
-b_theta <- 0.02
-a_sigma <- 1
-b_sigma <- 1
-theta <- 100
-lambda <- log(theta)
-sigma <- 0.6
-n <- 50000
-k <- 619
-m1 <- 500
-beta <- 0.5
-m1_bar <- runif(1,0,m1)
-a_beta <- 1
-b_beta <- 1
-#a_beta_post <- a_beta_prior+n-m1_bar
-#b_beta_post <- b_beta_prior+m1_bar
-alpha <- 10
+m1_bar <- round(runif(1,0,m1),digits=0)
 
 
-## UPDATE SIGMA
-f_sigma = function(sigma, theta, m1_bar){
-  psi <- log(sigma/(1-sigma))
-  S = 0
-  for (i in (m1+1):k){
-    S = S + lgamma(N_k[i] - sigma)
-  }
-  res = (a_sigma+k-m1_bar-1)*psi - (a_sigma+b_sigma+k-m1_bar-2)*log(1+exp(psi)) +
-      lgamma(a_sigma+b_sigma) - lgamma(a_sigma) - lgamma(b_sigma) + S -
-      (k-m1-1)*lgamma(1-sigma) + lgamma(theta/sigma+k-m1_bar) - lgamma(theta/sigma)
-  res
-}
+#sigma <- 0.17
+#psi <- log(sigma)-log(1-sigma)
 
-mh_sigma=function(iters, alpha, sigma, theta, m1_bar)
-{
-  xvec=numeric(iters)
+#theta <- 25
+#lambda <- log(theta)
 
-  for (i in 1:iters) {
-    sigma_s = sigma + runif(1,0,alpha)
-    A = f_sigma(sigma_s, theta, m1_bar)/f_sigma(sigma, theta, m1_bar)
-    if (runif(1) < A)
-      sigma = sigma_s
-    xvec[i] = sigma
+a <- list(theta = 2, sigma = 1, beta=1)
+b <- list(theta = 0.02, sigma = 1, beta=1)
+
+######## FUNZIONI SIGMA ########
+
+f_psi = function(lambda,psi,m1_bar){
+  S=0
+  for(j in 1:(k-m1)){
+    temp = lgamma(N_k[j+m1] - (exp(psi)/(1 + exp(psi)))) - lgamma(1/(1+exp(psi)))
+    S = S + temp
   }
   
-  return(xvec)
+  val = dbeta((exp(psi)/(1+exp(psi))),a$sigma,b$sigma, log=TRUE) +
+    ((k - m1_bar)*(psi - log(1 + exp(psi)))) + S + 
+    lgamma((exp(lambda)/exp(psi)) + exp(lambda) + k - m1_bar) -
+    lgamma((exp(lambda)/exp(psi)) + exp(lambda))
+  
+  return(val)  
 }
 
-post_s = mh_sigma(n, alpha, sigma, theta, m1_bar)
-hist(post_s,100,freq=FALSE, main="mh_sigma")
-curve(dbeta(x,a_sigma,b_sigma),add=TRUE,col=2,lwd=2)
-
-
-## UPDATE THETA
-
-f_theta = function(x, lambda, psi, m1_bar){
-  num = lgamma(lambda)+lgamma((lambda/psi)+k-m1_bar)
-  den = lgamma(lambda/psi)+lgamma(lambda+n-m1_bar)
-  res = dgamma(x,a_theta,b_theta)*(num-den)
-  res
-}
-
-mh_theta = function(iters,alpha, lambda, psi, m1_bar)
-{
-  xvec = numeric(iters)
-  x = 1
-  for (i in 1:iters) {
-    xs = x+rnorm(1,0,alpha)
-    A = f_theta(xs, lambda, psi, m1_bar)/f_theta(x, lambda, psi, m1_bar)
-    if (runif(1)<A)
-      x = xs
-    xvec[i] = x
+mh_psi = function(R,alpha,lambda,psi,m1_bar){
+  psi_vec = numeric(R)
+  accept=0
+  
+  for(i in 1:R){
+    
+    psi_s = rnorm(1,psi,alpha)
+    
+    A = min(1,exp( f_psi(lambda,psi_s,m1_bar)   
+                   - f_psi(lambda,psi,m1_bar) ))
+    
+    if(runif(1) < A){
+      psi = psi_s
+      accept = accept+1
+    }
+    psi_vec[i] = psi
   }
-  return(xvec)
+  
+  return(list(a=psi_vec,b=accept))
+  
 }
 
-#post_t = mh_theta(n,alpha, lambda, psi, m1_bar)
-#hist(post_t,100,freq=FALSE,main="mh_theta")
-#curve(dgamma(x,a_theta,b_theta),add=TRUE,col=2,lwd=2)
+######## FUNZIONI THETA #########
 
-
-# beta e m1 sampliamo solo un valore random?
-R = 1
-
-### valori iniziali
-lambda_old = lambda
-psi_old = psi
-beta_old = beta
-m1_bar_old = m1_bar
-
-for (r in 1:R){
+g_lambda = function(lambda,psi,m1_bar){
   
-  # update sigma
-  psi_new = mh_sigma(n, alpha, lambda_old, psi_old, m1_bar_old)
-  psi_new = mean(psi_new)
+  val = dgamma(exp(lambda),a$theta,rate=b$theta,log=TRUE) + 
+    lgamma(exp(lambda)) +
+    lgamma((exp(lambda)/exp(psi)) + exp(lambda) + k - m1_bar) -
+    lgamma((exp(lambda)/exp(psi)) + exp(lambda)) -
+    lgamma(exp(lambda) + n - m1_bar)
   
-  # NB: rising factorial -> (1-sigma)_(1-n(i)) = gamma(1-n(i))/(1-sigma)
-  # chi sono questi benedetti n(i)??
-  
-  # update theta
-  lambda_new = mh_theta(n, alpha, lambda_old, psi_new, m1_bar_old)
-  lambda_new = mean(lambda_new)
-  
-  # update beta -> Expected values of beta
-  #beta_new = (a_beta + n - m1_bar_old)/(a_beta + n + b_beta)
-  beta_new = beta_old
-  
-  # update m1_bar
-  num = lgamma((lambda_new/psi_new) + k - m1_bar_old)
-  den = lgamma(lambda_new + n - m1_bar_old)
-  
-  m1_bar_old = round(m1_bar_old, digits = 0)
-  
-  m1_bar_new = log(choose(m1,m1_bar_old)) + log(beta_new)*(n-m1_bar_old) + log((1-beta_new))*(m1_bar_old) + psi_new*(k-m1_bar_old) + num - den
-  m1_bar_new = exp(m1_bar_new)
-  #m1_bar_new = (choose(m1,m1_bar_old))*(beta_new^(n-m1_bar_old))*((1-beta_new)^(m1_bar_old))*(exp(psi_new)^(k-m1_bar_old))*num/den
-  
-  lambda_old <- lambda_new
-  psi_old <- psi_new
-  beta_old <- beta_new
-  m1_bar_old <- m1_bar_new
+  return(val)
 }
 
+mh_lambda = function(R,alpha,lambda,psi,m1_bar){
+  lambda_vec = numeric(R)
+   accept = 0
+  
+  for(i in 1:R){
+    
+    lambda_s = rnorm(1,lambda,alpha)
+    
+    A = min(1,exp(  g_lambda(lambda_s,psi,m1_bar)  
+                    - g_lambda(lambda,psi,m1_bar) ))
+    
+    if(runif(1) < A){
+      lambda = lambda_s
+       accept = accept + 1
+    }
+    lambda_vec[i] = lambda
+  }
+  
+  return(list(a=lambda_vec,b=accept))
+  
+}
+
+R <- 350
+it <- 500
+alpha <- list(theta = 1.5, sigma = 0.2)
+
+psi_vec_2 <- numeric(R)
+sigma_accept <- numeric(R)
+lambda_vec_2 <- numeric(R)
+theta_accept <- numeric(R)
+beta_vec <- numeric(R)
+m1_bar_vec <- numeric(R)
+
+psi_old <- rnorm(1,log(0.5)-log(0.5),alpha$sigma)
+lambda_old <- rnorm(1,log(1),alpha$theta)
+beta_old <- rbeta(1,a$beta,b$beta)
+m1_bar_old <- m1_bar
+
+
+for (i in 1:R){
+
+psi_new = mean(mh_psi(it,alpha$sigma,lambda_old,psi_old,m1_bar_old)$a)
+sigma_accept[i] = mh_psi(it,alpha$sigma,lambda_old,psi_old,m1_bar_old)$b/it
+
+lambda_new = mean(mh_lambda(it,alpha$theta,lambda_old,psi_new,m1_bar_old)$a)
+theta_accept[i] = mh_lambda(it,alpha$theta,lambda_old,psi_new,m1_bar_old)$b/it
+
+beta_new = rbeta(1, a$beta + n - m1_bar_old, b$beta + m1_bar_old)
+
+m1_bar_new = exp(lchoose(m1,m1_bar_old)) * beta_new^(n-m1_bar_old) * (1-beta_new)^(m1_bar_old) *
+             (exp(psi_new)/(1+exp(psi_new)))^(k-m1_bar_old) * 
+             exp(lgamma((exp(lambda_new)/exp(psi_new)) + exp(lambda_new) + k - m1_bar_old) -
+             lgamma(exp(lambda_new) + n - m1_bar_old))
+             
+
+psi_vec_2[i] = psi_new
+lambda_vec_2[i] = lambda_new
+beta_vec[i] = beta_new
+m1_bar_vec[i] = m1_bar_new
+
+psi_old = psi_new
+lambda_old = lambda_new
+beta_old = beta_new
+m1_bar_old = round(m1_bar_new,digits=0)
+
+}
+
+
+hist(psi_vec_2,100)
+hist(lambda_vec_2,100)
+hist(beta_vec,100)
+hist(m1_bar_vec,100)
+
+plot(psi_vec_2,type="l")
+plot(lambda_vec_2,type="l")
+plot(beta_vec,type='l')
+plot(m1_bar_vec, type='l')
+
+psi = mean(psi_vec_2)
+lambda = mean(lambda_vec_2)
+
+sigma = exp(psi)/(1+exp(psi))
+theta = exp(lambda)
+beta = mean(beta_vec)
+m1_bar = mean(m1_bar_vec)
+
+param = list(sigma=sigma,theta=theta,beta=beta,m1_bar=m1_bar)
+param
 
