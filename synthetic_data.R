1d3ba53a905756a4d52187a5f400348924b6d9c1
mtracanella <119503147+mtracanella@users.noreply.github.com>
2022-12-12 09:40:49 +0100

    Update synthetic_data.R

--- synthetic_data.R
+++ synthetic_data.R
@@ -5,99 +5,100 @@
 #
 library(dplyr)
 
-n <- 50000
+n <- 100000
 X <- numeric(n)
-beta <- 0.9
+J <- numeric(n)
+
+beta <- 0.95
 sigma <- 0.2
 theta <- 100
-J <- numeric(n)
-X[1] <- 1
-J[1] <- 1 #inizializzo J[1] con 1 perchè con J[1]=0 c'erano problemi 
-          # nel ciclo for perchè rimaneva bloccato nel while
-dummy <- 2
+
+clust <- 0
 m1 <- 0
+m1_bar <- 0
+m1_tilde <- 0
+
+a <- numeric(n)
+b <- numeric(n)
+c <- numeric(n)
 
 
-for (i in 2:n){
-  m1_mean <- m1*(1-beta) # media di una binomiale
-  u <- runif(1,0,1)
-  v <- runif(1,0,1)
+for (i in 1:n){
   
-  if (u>beta){
+  m1 <- m1_bar + m1_tilde
+  
+  a[i] <- (theta + (clust - m1_bar)*sigma)/(theta + i - m1_bar)
+  b[i] <- (m1_tilde*(1 - sigma))/(theta + i - m1_bar)
+  c[i] <- ((clust - m1)*sigma + n - m1_bar)/(theta + i - m1_bar)
+  
+  u <- runif(1,0,1)
+  
+  if (u > beta){
     # 1: specie contaminante nuova
-    X[i] <- dummy
+    X[i] <- clust + 1
     J[i] <- 0
-    dummy <- dummy+1
-    m1 <- m1+1
+    clust <- clust + 1
+    m1_bar <- m1_bar + 1
   }
-  if (u <= beta){
-    if (v < ((theta + (dummy-m1_mean)*sigma) / (theta+n-m1_mean))){
+  else{
+    v <- runif(1,0,1)
+    
+    if (v < a[i]){
       # 2: specie non contaminante nuova
-      X[i] <- dummy
+      X[i] <- clust + 1
       J[i] <- 1
-      dummy <- dummy+1
-      m1 <- m1+1
+     clust <- clust + 1
+      m1_tilde <- m1_tilde + 1
     }
     else{
-      t <- round(runif(1, 1, i),digits=0) # genera un indice tra quelli disponibili
-      while (J[t]==0){
-        t <- round(runif(1, 1, i),digits=0)
-        # rifalla finché non ho un valore non contaminante
-      }
-      
-      # 3: non contaminante, freq 1
-      if ((length(which(X == X[t])))==1){ # se quel valore l'ho già incontrato solo una volta
+     
+      if (v < (a[i] + b[i])){
+        # 3: non contaminante, freq 1
+        t <- round(runif(1, 1, clust),digits=0) # genera un indice tra quelli disponibili
+        while (J[t] == 0 && length(X[t])>1){
+          t <- round(runif(1, 1, clust),digits=0)
+        }
+        
         X[i] <- X[t]
-        m1 <- m1-1
+        m1_tilde <- m1_tilde - 1
         J[i] <- 1
       }
       else{
-      # 4: non contaminante, freq > 1
-      if (((length(which(X == X[t])))>1)){ # se quel valore l'ho già incontrato più volte
-        X[i] <- X[t]
-        J[i] <- 1
-      }
+        # 4: non contaminante, freq > 1
+        t <- round(runif(1, 1, clust),digits=0) # genera un indice tra quelli disponibili
+        while (J[t] == 0 && length(X[t])==1){
+          t <- round(runif(1, 1, clust),digits=0)
+        }
+          X[i] <- X[t]
+          J[i] <- 1
+        }
       }
       
     }
   }
   
-  # taglio di J, ovvero
-  # controlla in X tutti quelli che hanno frequenza >1 e elimina i valori di J in tutti gli
-  # indici dove compare quel numero
-  
-}
 
 # lego i gruppi a J
 data = as.data.frame(cbind(X,J))
 
-# Trovo quanti sono i dati contaminati che rappresentano m1_bar
-ind = which(data$J==0)
-m1_cont = length(ind)
-
 # rimuovo dal dataset i singleton contaminanti
+ind = which(data$J==0)
 data_no_cont = data[-ind,]
 
 # trovo la numerosità di ogni gruppo
 subset = count(data_no_cont,X)
 
-# Trovo i singleton non contaminanti
-ind_2 = which(subset$n==1)
-m1_no_cont = length(ind_2)
-
 # Rimuovo i singleton non cantaminanti dal dataset e genero il vettore con le numerosità dei gruppi
+ind_2 = which(subset$n==1)
 subset_2 = subset[-ind_2,]
 N_k = subset_2$n
 N_k = sort(N_k)
-
-# numero di singleton
-m1 = m1_cont + m1_no_cont
 
 # numero di gruppi
 k = m1 + length(N_k)
 
 # salvo tutti i parametri che mi servono per controllare se il modello funziona
-par = list(n=n,m1=m1,m1_bar=m1_cont,N_k=N_k,k=k,beta=beta,sigma=sigma,theta=theta)
+par = list(n=n, m1=m1, m1_bar=m1_bar, N_k=N_k, k=k, beta=beta, sigma=sigma, theta=theta)
 par
 
 save(par, file = "par.RData")
