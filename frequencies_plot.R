8dba205c1864a817f3991f9908dfe969f89674ce
vivigiorgi <119507927+vivigiorgi@users.noreply.github.com>
2023-01-25 16:15:45 +0100

    Add files via upload

---
+++ frequencies_plot.R
@@ -0,0 +1,12 @@
+library(dplyr)
+j = 2
+data = read.csv(paste0("synthetic_data_", j,".csv"), header = TRUE)
+n = dim(data)[1]
+
+subset = count(data, data[2])[2]
+N_k = subset$n #frequenze gruppi
+N_k = sort(N_k)
+k = length(N_k)
+
+fr = as.data.frame(table(as.data.frame(N_k)))
+plot(log(as.numeric(fr$N_k)),log(fr$Freq),xlab="n_j(log-scale)",ylab="m_j(log-scale)")
