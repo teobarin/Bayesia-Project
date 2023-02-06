15b87896d7460a911dc3d4fc8d4955835f2eaa8b
mtracanella <119503147+mtracanella@users.noreply.github.com>
2023-02-06 23:01:41 +0100

    Update par_estimates.R

--- par_estimates.R
+++ par_estimates.R
@@ -1,8 +1,21 @@
 library(dplyr)
 library(readr)
 
-load("C:/Users/marco/Desktop/Univerisità/Bayesian Statistics/Progetto/freq.RData")
-load("C:/Users/marco/Desktop/Univerisità/Bayesian Statistics/Progetto/functions.RData")
+# - freq.RData is the file containing the frequencies of the clusters of our dataset
+# - functions.RData is the file containing all functions that we use to estimate parameters
+# - sigma_par is a list containing the parameters of the prior distribution of sigma
+# - theta_par is a list containing the parameters of the prior distribution of theta
+# - beta_par is a list containing the parameters of the prior distribution of beta
+# - alpha is a list containing the variance of the proposal distribution that we use
+#   in the Metropolis-Hastings to update sigma and theta
+# - iters are the number of iteration of our sampling
+# - cPY_freq is the function that returned the vectors containing the values 
+#   of the parameters sampled with the contaminated Pitma-Yor updating functions
+# - PY_freq is the function that returned the vectors containing the values 
+#   of the parameters sampled with the Pitma-Yor updating functions   
+
+load("C:/.../freq.RData")
+load("C:/.../functions.RData")
 
 sigma_par = list(a=1,b=1)
 theta_par = list(a=2,b=0.02)
